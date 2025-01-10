579094963238c2c36b321a63edaba451a7c50dfc
Hoverok <pavelchi91@gmail.com>
2025-01-11 01:31:39 +0200

    accordion example (html, css, js)

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+const accordionItemElements = document.querySelectorAll(".item");
+console.log(accordionItemElements);
+
+const openAccordionItem = function () {
+  accordionItemElements.forEach((item) => {
+    item.addEventListener("click", function (e) {
+      const clicked = e.target.closest(".hidden-box");
+      if (clicked) return;
+      [...item.parentElement.children].forEach((child) => {
+        if (child !== item) child.classList.remove("open");
+      });
+      item.classList.toggle("open");
+    });
+  });
+};
+
+openAccordionItem(accordionItemElements);
